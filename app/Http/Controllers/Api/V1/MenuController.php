aa9d2d75892259923462a114c364d8fd563ab131
luoxu <>
2018-10-20 01:19:58 +0800

    menu

---
+++ app/Http/Controllers/Api/V1/MenuController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luoxulx
+ * Date: 2018/10/20
+ * Time: 01:19
+ */
+
+namespace App\Http\Controllers\Api\V1;
+
+
+class MenuController extends ApiController
+{
+
+}
