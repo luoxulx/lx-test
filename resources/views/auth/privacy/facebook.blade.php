3b40029bac1592dfa7dedc1432313aa3b0dcb572
14k <longkuilx@gmail.com>
2018-12-13 21:25:50 +0800

    privacy-test

--- resources/views/auth/privacy/facebook.blade.php
+++ resources/views/auth/privacy/facebook.blade.php
@@ -1,17 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
-
-    <title>PrivacyPolicyView--{{ config('app.name') }}</title>
-</head>
-
-<div class="container-fluid">
+@extends('auth.privacy.parent')
+@section('main')
     <div class="row">
         <div class="col-md-8">
             <h2 class="_353a _52ip _52id _3m9" style="font-size: 40px; margin: 0px; padding: 0px 0px 19px; line-height: 48px; text-rendering: optimizelegibility; font-family: inherit; font-weight: normal !important;"> Data Policy </h2>
@@ -136,11 +124,4 @@
         </div>
         <div class="col-md-4"></div>
     </div>
-</div>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.slim.min.js"></script>
-<script src="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
-</body>
-</html>
+@endsection
---
+++ resources/views/auth/privacy/parent.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>PrivacyPolicy--{{ config('app.name') }}</title>
+</head>
+
+<div class="container-fluid">
+    @yield('main')
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.slim.min.js"></script>
+<script src="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
